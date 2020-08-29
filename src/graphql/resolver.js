3c1d7788cbab73e66fe32bc776b284293e3d71fd
Robert Damaceno <robertdamacenobob@gmail.com>
2020-08-29 20:45:18 -0300

    :boom: aprendendo a fazer middleware em pleno sábado não deu certo

--- src/graphql/resolver.js
+++ src/graphql/resolver.js
@@ -6,8 +6,22 @@ const config = require('../config/config')
 const User = require('../models/User')
 const Post = require('../models/Post')
 
+function verifyAuth(token) {
+    jwt.verify(token, config.secret, (err, decoded) => {
+        if (err) return false
+
+        return true
+    })
+}
+
 const resolvers = {
-    hello: User.find(),
+    posts: (args) => {
+        const { token } = args
+
+        verifyAuth(token)
+
+        return Post.find()
+    },
 
     createUser: async (args) => {
         const { name, email, password } = args
@@ -20,7 +34,7 @@ const resolvers = {
 
         const user = await User.create(newUser)
 
-        return jwt.sign({id: user._id}, config.secret, {
+        return jwt.sign({ id: user._id }, config.secret, {
             expiresIn: 604800
         })
     },
@@ -31,17 +45,17 @@ const resolvers = {
         const user = await User.findOne({ email }).select('+password')
 
         if (!!user && await bcrypt.compare(password, user.password)) {
-            return jwt.sign({id: user._id}, config.secret, {
+            return jwt.sign({ id: user._id }, config.secret, {
                 expiresIn: 604800
             })
         } return 401
     },
 
     createPost: async (args) => {
-        const { authorID, author, content} = args
+        const { authorID, author, content } = args
 
         const id = jwt.verify(authorID, config.secret, (err, decoded) => {
-            if(err) return null 
+            if (err) return null
 
             return decoded.id
         })
